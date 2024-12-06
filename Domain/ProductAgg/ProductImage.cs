6e5b26915d4c6635d88883c31342546215d47215
ArshamCoder <arshambh7@gmail.com>
2024-12-06 11:40:46 +0330

    ProductAgg

---
+++ Domain/ProductAgg/ProductImage.cs
@@ -0,0 +1,20 @@
+﻿using Comman.Domain.Exceptions;
+using Common.Domain;
+
+namespace Domain.ProductAgg
+{
+    public class ProductImage : BaseEntity
+    {
+        public ProductImage(string imageName, int sequence)
+        {
+            NullOrEmptyDomainDataException.CheckString(imageName, nameof(imageName));
+
+            ImageName = imageName;
+            Sequence = sequence;
+        }
+
+        public long ProductId { get; internal set; }
+        public string ImageName { get; private set; }
+        public int Sequence { get; private set; }
+    }
+}
