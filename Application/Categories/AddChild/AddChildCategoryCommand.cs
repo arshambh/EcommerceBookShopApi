ea733374d44338417cb1ad9a4ed1656f2c412e58
ArshamCoder <arshambh7@gmail.com>
2024-12-09 08:44:02 +0330

    upgrade to dotnet 9 - Category Use Cases

---
+++ Application/Categories/AddChild/AddChildCategoryCommand.cs
@@ -0,0 +1,6 @@
+﻿using Common.Domain.ValueObjects;
+
+namespace Application.Categories.AddChild
+{
+    public record AddChildCategoryCommand(long ParentId, string Title, string Slug, SeoData SeoData) : Common.Application.IBaseCommand;
+}
