2f124b51ab72afa464a6bdb400bb3fe2e2aa5315
ArshamCoder <arshambh7@gmail.com>
2024-12-10 07:18:23 +0330

    Comments

---
+++ Application/Comments/ChangeStatus/ChangeCommentStatusCommand.cs
@@ -0,0 +1,7 @@
+﻿using Common.Application;
+using Domain.CommentAgg;
+
+namespace Application.Comments.ChangeStatus
+{
+    public record ChangeCommentStatusCommand(long Id, CommentStatus Status) : IBaseCommand;
+}
